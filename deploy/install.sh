86f412a41ec8ab191a4ffcc869de08cfdadd9f82
Seán C McCord <ulexus@gmail.com>
2023-10-02 20:00:26 -0400

    clean up init container build

--- deploy/install.sh
+++ deploy/install.sh
@@ -5,6 +5,3 @@ set -e
 mkdir -p /plugins-local/src/github.com/nscuro/traefik-plugin-geoblock
 cp -R /dist/* /plugins-local/src/github.com/nscuro/traefik-plugin-geoblock/
 cp /dist/.traefik.yml /plugins-local/src/github.com/nscuro/traefik-plugin-geoblock/
-
-# Deploy directory is neither needed nor wanted
-rm -Rf /plugins-local/src/github.com/nscuro/traefik-plugin-geoblock/deploy
